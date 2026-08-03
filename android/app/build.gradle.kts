fd836b9a8a3e62096c9eb76aaf24811c6019925c
Sulaiman507 <slymanalrmady@gmail.com>
2026-08-03 03:15:51 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -10,7 +9,6 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        // تفعيل Desugaring لحل مشكلة مكتبة الإشعارات
         isCoreLibraryDesugaringEnabled = true
 
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -18,20 +16,18 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.prays_times_kimi"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        // دعم MultiDex لحل مشكلة توقف البناء مع مكتبات الإشعارات
+        multiDexEnabled = true
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -48,6 +44,5 @@ flutter {
 }
 
 dependencies {
-    // مكتبة Desugaring المطلوبة لدعم الإشعارات على أندرويد
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
